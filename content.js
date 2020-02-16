6dc8432c8285b348f5e162fef76d466604fd65b6
knnan <28503456+knnan@users.noreply.github.com>
2020-02-17 02:25:25 +0530

    made version 2 of image extractor

--- content.js
+++ content.js
@@ -1,128 +1,29 @@
 console.log("content script is running");
-
-var pageNo = 1;
-var url;
-var origin;
-var username;
-var csrf;
-var referer;
-var offset;
-var limit;
-var dapiid;
-var result = [];
-var images = [];
-var src = [];
-var altmain = [];
-var alt2 = [];
-
-
-
-let imgprev = document.querySelectorAll('[data-sigil="torpedo-img"]');
-for (let i = 0; i < imgprev.length; i++)
-{
-    if (src.length > 0)
-    {
-        if (!src.includes(imgprev[ i ].src.split('/v1')[ 0 ]))
-        {
-            src.push(imgprev[ i ].src.split('/v1')[ 0 ]);
-            altmain.push(imgprev[ i ].alt.split('by')[ 0 ])
-        }
-    }
-    else
-    {
-        src.push(imgprev[ i ].src.split('/v1')[ 0 ]);
-        altmain.push(imgprev[ i ].alt.split('by')[ 0 ])
-
-    }
-}
-chrome.runtime.onMessage.addListener(gotMessage);
-
-function gotMessage (message, sender, sendResponnse)
-{
-
-    url = message.headers.url.split('mp=')[ 0 ];
-    origin = message.headers.requestHeaders[ 0 ].value;
-    // referer = message.headers.requestHeaders[ 4 ].value;
-    username = message.request.requestBody.formData.username[ 0 ];
-    limit = message.request.requestBody.formData.limit[ 0 ];
-    offset = message.request.requestBody.formData.offset[ 0 ];
-    csrf = message.request.requestBody.formData._csrf[ 0 ];
-    dapiid = message.request.requestBody.formData.dapiIid[ 0 ];
-    checglb();
-}
-async function checglb ()
-{
-    var bodystring = `username=${username}&offset=`;
-    var bodystring2 = `&limit=${limit}&_csrf=${csrf}&dapiIid=${dapiid}`;
-
-
-    var pages = 1;
-    while (true)
-    {
-
-        let res = await fetch(url + `=${String(pages)}`, {
-            "headers": {
-                "accept": "*/*",
-                "accept-language": "en-US,en;q=0.9",
-                "content-type": "application/x-www-form-urlencoded"
-            },
-            // "referrer": referer,
-            "referrerPolicy": "no-referrer-when-downgrade",
-            "body": `${bodystring}${offset}${bodystring2}`,
-            "method": "POST",
-            "mode": "cors"
-        });
-        let data = await res.json();
-        console.log({
-            resno: pages,
-            data: data
-        });
-        offset = String(data.content.next_offset);
-        result = result.concat(data.content.results);
-        if (!data.content.has_more == true) break; //Were done let's stop this thing
-
-    }
-
-    for (i = 0; i < result.length; i++)
-    {
-        var temp = null;
-        temp = document.createElement('div');
-        temp.innerHTML = result[ i ].html;
-        try
-        {
-            obj = {
-                src: temp.querySelector('[data-sigil="torpedo-img"]').src.split('/v1')[ 0 ],
-                alt: temp.querySelector('[data-sigil="torpedo-img"]').alt.split('by')[ 0 ]
-            }
-            images.push(temp.querySelector('[data-sigil="torpedo-img"]').src.split('/v1')[ 0 ]);
-            alt2.push(temp.querySelector('[data-sigil="torpedo-img"]').alt.split('by')[ 0 ]);
-        } catch (err)
-        {
-            console.log(err);
-        }
-    }
-    for (let i = 0; i < images.length; i++)
-    {
-        if (src.length > 0)
-        {
-            if (!src.includes(images[ i ]))
-            {
-                src.push(images[ i ]);
-                altmain.push(alt2[ i ]);
-            }
-
-        }
-        else
-        {
-            src.push(images[ i ]);
-        }
-    }
-
-
-    chrome.runtime.sendMessage({
+console.log("test")
+chrome.runtime.onMessage.addListener(
+  function(msg, sender, sendResponse) {
+    console.log("get the falsdkjljfdsalds")
+    console.log(msg);
+     chrome.runtime.sendMessage({
         data: {
-            src: src,
-            titles: altmain
+            src: msg,
+          
+
+        }
+    }, function (response)
+        {
+            console.dir(response);
+        });
+
+  });
+
+
+function sendMessagetopopup(msg)
+{
+     chrome.runtime.sendMessage({
+        data: {
+            src: msg,
+          
 
         }
     }, function (response)
@@ -130,3 +31,134 @@ async function checglb ()
             console.dir(response);
         });
 }
+// var pageNo = 1;
+// var url;
+// var origin;
+// var username;
+// var csrf;
+// var referer;
+// var offset;
+// var limit;
+// var dapiid;
+// var result = [];
+// var images = [];
+// var src = [];
+// var altmain = [];
+// var alt2 = [];
+
+
+
+// let imgprev = document.querySelectorAll('[data-sigil="torpedo-img"]');
+// for (let i = 0; i < imgprev.length; i++)
+// {
+//     if (src.length > 0)
+    
+// {        if (!src.includes(imgprev[ i ].src.split('/v1')[ 0 ]))
+//         {
+//             src.push(imgprev[ i ].src.split('/v1')[ 0 ]);
+//             altmain.push(imgprev[ i ].alt.split('by')[ 0 ])
+//         }
+//     }
+//     else
+//     {
+//         src.push(imgprev[ i ].src.split('/v1')[ 0 ]);
+//         altmain.push(imgprev[ i ].alt.split('by')[ 0 ])
+
+//     }
+// }
+// chrome.runtime.onMessage.addListener(gotMessage);
+
+// function gotMessage (message, sender, sendResponnse)
+// {
+
+// console.log('message received ',message);
+//     url = message.headers.url.split('mp=')[ 0 ];
+//     origin = message.headers.requestHeaders[ 0 ].value;
+//     // referer = message.headers.requestHeaders[ 4 ].value;
+//     username = message.request.requestBody.formData.username[ 0 ];
+//     limit = message.request.requestBody.formData.limit[ 0 ];
+//     offset = message.request.requestBody.formData.offset[ 0 ];
+//     csrf = message.request.requestBody.formData._csrf[ 0 ];
+//     dapiid = message.request.requestBody.formData.dapiIid[ 0 ];
+//     checglb();
+// }
+// async function checglb ()
+// {
+//     var bodystring = `username=${username}&offset=`;
+//     var bodystring2 = `&limit=${limit}&_csrf=${csrf}&dapiIid=${dapiid}`;
+
+
+//     var pages = 1;
+//     while (true)
+//     {
+
+//         let res = await fetch(url + `=${String(pages)}`, {
+//             "headers": {
+//                 "accept": "*/*",
+//                 "accept-language": "en-US,en;q=0.9",
+//                 "content-type": "application/x-www-form-urlencoded"
+//             },
+//             // "referrer": referer,
+//             "referrerPolicy": "no-referrer-when-downgrade",
+//             "body": `${bodystring}${offset}${bodystring2}`,
+//             "method": "POST",
+//             "mode": "cors"
+//         });
+//         let data = await res.json();
+//         console.log({
+//             resno: pages,
+//             data: data
+//         });
+//         offset = String(data.content.next_offset);
+//         result = result.concat(data.content.results);
+//         if (!data.content.has_more == true) break; //Were done let's stop this thing
+
+//     }
+
+//     for (i = 0; i < result.length; i++)
+//     {
+//         var temp = null;
+//         temp = document.createElement('div');
+//         temp.innerHTML = result[ i ].html;
+//         try
+//         {
+//             obj = {
+//                 src: temp.querySelector('[data-sigil="torpedo-img"]').src.split('/v1')[ 0 ],
+//                 alt: temp.querySelector('[data-sigil="torpedo-img"]').alt.split('by')[ 0 ]
+//             }
+//             images.push(temp.querySelector('[data-sigil="torpedo-img"]').src.split('/v1')[ 0 ]);
+//             alt2.push(temp.querySelector('[data-sigil="torpedo-img"]').alt.split('by')[ 0 ]);
+//         } catch (err)
+//         {
+//             console.log(err);
+//         }
+//     }
+//     for (let i = 0; i < images.length; i++)
+//     {
+//         if (src.length > 0)
+//         {
+//             if (!src.includes(images[ i ]))
+//             {
+//                 src.push(images[ i ]);
+//                 altmain.push(alt2[ i ]);
+//             }
+
+//         }
+//         else
+//         {
+//             src.push(images[ i ]);
+//         }
+//     }
+
+
+//     chrome.runtime.sendMessage({
+//         data: {
+//             src: src,
+//             titles: altmain
+
+//         }
+//     }, function (response)
+//         {
+//             console.dir(response);
+//         });
+// }
--- popup.js
+++ popup.js
@@ -1,19 +1,45 @@
 var msg = {
     headers: undefined,
     request: undefined,
-    response: undefined
+    response: undefined,
+    final_data:undefined
 };
 var Folder;
-var glbmessage;
-
+var glbmessage = {};
+function downloadit(dourl)
+{
+  chrome.downloads.download({
+                url: String(dourl),
+                saveAs: false
+            });
+}
 
 
 document.getElementById('refresh').addEventListener('click', (e) => {
-
+let d = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/40b917ca-54f0-4d4a-b0e7-b3034357dc15/d6v5rki-0d8b2f93-34a1-4529-a88d-3b0f2f39d797.jpg/v1/fit/w_150,h_150,q_70,strp/tentacel_beast_by_pyxartz_d6v5rki-150.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA4MCIsInBhdGgiOiJcL2ZcLzQwYjkxN2NhLTU0ZjAtNGQ0YS1iMGU3LWIzMDM0MzU3ZGMxNVwvZDZ2NXJraS0wZDhiMmY5My0zNGExLTQ1MjktYTg4ZC0zYjBmMmYzOWQ3OTcuanBnIiwid2lkdGgiOiI8PTE5ODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.GAED2QZOrv9eIll6UsDeN2_v8voB3z5KtgJ_OgeurJw";
+  chrome.downloads.download({
+                url: d,
+                saveAs: false
+            });
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs)
     {
+        console.log("sldjfkjlasdjljsd;j;lsjdfjl;jasldjlksjdflk")
         chrome.tabs.update(tabs[ 0 ].id, { url: tabs[ 0 ].url });
+
+        // chrome.tabs.reload(tabs[0].id);
     });
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs)
+    {
+        console.log("Reloading the tab")
+        chrome.tabs.update(tabs[ 0 ].id, { url: tabs[ 0 ].url });
+
+        // chrome.tabs.reload(tabs[0].id);
+    });
+    console.log('DOM fully loaded and parsed');
 });
 
 document.querySelector('form').addEventListener('submit', (e) =>
@@ -23,8 +49,8 @@ document.querySelector('form').addEventListener('submit', (e) =>
     Folder = String(formData.get('subfolder'))+"/";
     e.preventDefault();
 });
+// chrome.downloads.onDeterminingFilename.addListener(getfiletype);
 
-chrome.downloads.onDeterminingFilename.addListener(getfiletype);
 function getfiletype (item, suggest)
 {
     console.log("This is the file type");
@@ -50,10 +76,9 @@ function getfiletype (item, suggest)
 
 
 
-
 // fuction to get the request HEADERS
 getHeaders = (filter = {
-    urls: [ "*://www.deviantart.com/dapi/*" ]
+    urls: [ "*://www.deviantart.com/*" ]
 }, options = [ "blocking", "requestHeaders" ]) =>
 {
     return new Promise((accept, reject) =>
@@ -63,16 +88,7 @@ getHeaders = (filter = {
 };
 
 
-// fuction to get the request BODY
-getResponse = (filter = {
-    urls: [ "*://www.deviantart.com/dapi/*" ]
-}, options = [ "blocking", "requestBody" ]) =>
-{
-    return new Promise((accept, reject) =>
-    {
-        chrome.webRequest.onBeforeRequest.addListener(accept, filter, options);
-    });
-};
+
 
 
 //function to get current Tab
@@ -87,41 +103,109 @@ getTab = (par = {
     });
 };
 
+
+
+async function getData()
+{
+     const [ Headers, tab ] = await Promise.all([ this.getHeaders(),this.getTab() ]);
+    msg.headers = Headers;
+    res = await axios.get("https://www.deviantart.com/pyxartz/gallery");
+    data = res.data;
+let doc = new DOMParser().parseFromString(data, 'text/html');
+
+const regExp = /\(([^);]+)\)/;
+let all_config_strings = doc.querySelectorAll('script')[10].text.split('\n');
+let config_string =  all_config_strings.filter( ele => {return ele.includes('window.__INITIAL_STATE__')});
+let init = config_string[0].indexOf('JSON.parse(');
+let fin = config_string[0].indexOf(');');
+let final_string = '"'+config_string[0].substr(init+12,fin-init-12)
+let answer = JSON.parse(JSON.parse(final_string));
+let devi  = answer['@@entities']['deviation']
+let all_src = [];
+let medias = []
+for(let d in devi)
+{
+media = devi[d].media;
+// console.log(media.token[0]);
+ let baseURL = media.baseUri;
+    let prettyName = media.prettyName;
+    let token = media.token[0];
+    let pre_type = await media.types.filter( async t => {return t.t == 'preview'});
+    // console.log(pre_type);
+    let type_name = pre_type[0].c.replace('<prettyName>',prettyName)
+
+
+    let URL = `${baseURL}/${type_name}?token=${token}`
+    src = {}
+    src.URL = URL;
+    src.title = media.prettyName.split('-')[0];
+    all_src.push(src);
+
+}
+msg.final_data = all_src;
+ chrome.tabs.sendMessage(tab[ 0 ].id, msg);
+    M.toast({ html: 'parsed!' })
+    return msg;
+
+}
+
 async function getDetails ()
 {
 
-    const [ Headers, Request, tab ] = await Promise.all([ this.getHeaders(), this.getResponse(), this.getTab() ]);
+    const [ Headers, tab ] = await Promise.all([ this.getHeaders(),this.getTab() ]);
     msg.headers = Headers;
+    await this.getData();
+
+    
     msg.request = Request;
+    console.log('dksla;fsjd')
+    console.log('final_result'.glbmessage);
+    msg.data = glbmessage;
+  
     chrome.tabs.sendMessage(tab[ 0 ].id, msg);
     M.toast({ html: 'parsed!' })
-    chrome.webRequest.onBeforeSendHeaders.removeListener(this.getHeaders);
-    chrome.webRequest.onBeforeRequest.removeListener(this.getResponse);
+    // chrome.webRequest.onBeforeSendHeaders.removeListener(this.getHeaders);
 }
-getDetails();
+// getData().then(res => console.log('ls msg is ',res)).catch(err => consoel.log(err));
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse)
 {
     glbmessage = message;
-    console.log(message);
-    console.log(message.data.src.length);
-    document.getElementById("totalimages").innerText = String(message.data.src.length);
+    console.log('Returned message from content',message);
+
+});
+
+
+async function testagain()
+{
+    let testresult = await getData();
+    console.log('aa test restu'.testresult);
+}
+
+
+
+function displayOutput(img_obj)
+{
+
+   img_src = img_obj.final_data;
+    document.getElementById("totalimages").innerText = String(img_src.length);
     console.log(document.getElementById("totalimages"));
     imagediv = document.getElementById('image-gallery');
     output1 = '';
     output = '';
-    for (let i = 0; i < message.data.src.length; i++)
+    for (let i = 0; i < img_src.length; i++)
     {
         output1 += `<div class="col s4">            
-                <img src=${message.data.src[ i ]}  class="responsive-img z-depth 4 "/>
+                <img src=${img_src[ i ].URL}  class="responsive-img z-depth 4 "/>
                 </div>`;
         output += `<div class="col s4">
 <div class="card ">
                 <div class="card-image">
-                    <img src=${message.data.src[ i ]}  class="downloadbutton1"   alt="${message.data.titles[ i ]}"  />
+                    <img src=${img_src[i].URL}  class="downloadbutton1"   alt="${img_src[i].title}"  />
                     
                 </div>
                  <a class="btn-floating halfway-fab waves-effect waves-light  "   >
-               <i class="tiny material-icons downloadbutton"   customattr=${message.data.src[ i ]}  imagename="${message.data.titles[ i ]}" >arrow_downward</i>
+               <i class="tiny material-icons downloadbutton"   customattr=${img_src[i].URL}  imagename="${img_src[i].title}" >arrow_downward</i>
                </a>
               
             </div>
@@ -151,14 +235,14 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse)
             var eventobj = e.target.attributes;
             url1 = eventobj.customattr.value;
 
-            
+            console.log(url1);
             console.log('download INitiated');
-            chrome.downloads.download({
-                url: String(url1),
-                saveAs: false
-            });
+            downloadit();
         });
     }
+
+
+
     document.getElementById('button2').addEventListener('click', (e) =>
     {
         images = document.querySelectorAll('img');
@@ -181,5 +265,8 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse)
     sendResponse({
         data: "I am fine, thank you. How is life in the background?"
     });
-});
 
+}
+getData().then(res => {
+displayOutput(res)
+}).catch(err =>console.log(err));
